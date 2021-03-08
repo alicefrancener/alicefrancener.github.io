ca333c4f5e29d8427a5536e31d52ddddefcb4eaa
Alice Francener <alicefrancener@gmail.com>
2021-03-08 15:37:20 -0300

    chore: update docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://alicefrancener.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
